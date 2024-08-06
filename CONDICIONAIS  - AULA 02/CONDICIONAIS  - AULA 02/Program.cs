3320ff9261006f7c6445d396b63be0f6be732e0d
Pedro Gustavo Brandão Sá Da Silva <140172385+Goleiro19@users.noreply.github.com>
2024-08-06 11:30:53 -0300

    Add files via upload

---
+++ CONDICIONAIS  - AULA 02/CONDICIONAIS  - AULA 02/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Title = "Nome da instituição";
+
+Console.WriteLine("Hello, Info07M!");
+
+String nome;
+String Disciplina;
+Double nota1, nota2, nota3, média;
+
+Console.WriteLine("Digite o nome do aluno: ");
+nome = Console.ReadLine();
+
+Console.WriteLine("Digite o nome da disciplina: ");
+Disciplina = Console.ReadLine();
+
+Console.WriteLine("Digite a nota1 do aluno: ");
+nota1 = Double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a nota2 do aluno: ");
+nota2 = Double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a nota3 do aluno: ");
+nota3 = Double.Parse(Console.ReadLine());
+
+média = (nota1 + nota2 + nota3) / 3;
+
+
+
+Console.WriteLine("Aguarde.....," + nome);
+
+Console.WriteLine("Média Final : " + média);
+
+if(média > 6)
+{
+    Console.WriteLine("Aprovado!!!");
+}
+else if (média == 6)
+{
+    Console.WriteLine("Aprovado na média");
+}
+else
+{
+    Console.WriteLine("Reprovado!!!");
+
+}
